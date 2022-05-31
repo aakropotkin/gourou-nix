3346bb3038376f5a5924ad470ed3a3ba9561e37b
Alex Ameen <alex.ameen.tx@gmail.com>
2022-05-30 19:29:25 -0500

    tried to get old flake running, I think upstream Musl might have blown up in the last few hours?

--- flake.nix
+++ flake.nix
@@ -1,172 +1,153 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils/master";
-    gourou-src = {
-      #url   = "github:BentonEdmondson/the-scroll/main";
-      url   = "git://soutade.fr/libgourou.git";
-      type  = "git";
-      ref   = "master";
-      flake = false;
-    };
-    updfparser-src = {
-      url   = "git://soutade.fr/updfparser.git";
-      type  = "git";
-      ref   = "master";
-      flake = false;
-    };
-    base64-src = {
-      url = "git+https://gist.github.com/f0fd86b6c73063283afe550bc5d77594.git";
-      flake = false;
-    };
-    pugixml-src = {
-      url   = "github:zeux/pugixml/latest";
-      flake = false;
-    };
+  description = "a basic package";
+
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  inputs.pugixml-src = {
+    url = "github:zeux/pugixml/latest";
+    flake = false;
+  };
+  inputs.base64-src = {
+    url = "git+https://gist.github.com/f0fd86b6c73063283afe550bc5d77594.git";
+    flake = false;
+  };
+  inputs.updfparser-src = {
+    url = "git://soutade.fr/updfparser";
+    flake = false;
+  };
+  inputs.gourou-src = {
+    url = "git://soutade.fr/libgourou.git";
+    type = "git";
+    flake = false;
   };
 
-  outputs = {
-    self
-  , nixpkgs
-  , flake-utils
-  , gourou-src
-  , updfparser-src
-  , base64-src
-  , pugixml-src
-  }:
-  let
-    supportedSystems = ["x86_64-linux"];
-    systemsMap = flake-utils.lib.eachSystemMap supportedSystems;
+  outputs = { self, nixpkgs, utils, pugixml-src, base64-src, updfparser-src, gourou-src }:
+  let systemMap = utils.lib.eachSystemMap utils.lib.defaultSystems;
   in {
-    packages = systemsMap ( system:
-      let
-        pkgsFor = import nixpkgs { inherit system; };
-        support = import ./support.nix {
-          inherit (pkgsFor) stdenv linkFarmFromDrvs;
-        };
+    packages = systemMap ( system:
+      let pkgsFor = import nixpkgs { inherit system; };
+      in rec {
 
-        base64 = derivation {
-          name = "base64";
-          inherit system;
-          builder = "${pkgsFor.bash}/bin/bash";
-          PATH = "${pkgsFor.coreutils}/bin";
-          args = ["-c" ''
-            mkdir -p $out/include/base64;
-            cp -p --reflink=auto  ${base64-src}/Base64.h "$out/include/base64/"
-          ''];
-        };
-
-        libupdfparser = support.mkCxxLibs {
-          name = "libupdfparser";
+        updfparser = pkgsFor.pkgsStatic.stdenv.mkDerivation {
+          pname = "updfparser";
+          version = "1.6.25-dev";
           src = updfparser-src;
+          enableDebug = false;
+          makeFlags = [
+            "BUILD_STATIC=1"
+            "BUILD_SHARED=0"
+          ];
+          configurePhase = ''
+            runHook preConfigure
+            : "''${enableDebug=0}"
+            test "$enableDebug" != 0 && makeFlagsArray+=( DEBUG=1 )
+            makeFlagsArray+=( CXXFLAGS="-I./include -static" )
+            runHook postConfigure
+          '';
+          checkPhase = ''
+            runHook preCheck
+            make test && ./test
+            runHook postCheck
+          '';
+          pkgConfigDef = ''
+            prefix=@out@
+            exec_prefix=''${prefix}
+            libdir=''${prefix}/lib
+            includedir=''${prefix}/include
+
+            Name: updfparser
+            Description: updfparser
+            Version: 1.6.25-dev
+            Libs: -L''${libdir} -lupdfparser
+            Cflags: -I''${includedir}
+          '';
+          passAsFile = ["pkgConfigDef"];
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib $out/lib/pkgconfig $dev
+            mv include $out/
+            mv -- libupdfparser.a $out/lib/
+            runHook postInstall
+          '';
         };
 
-        libgourou = support.mkCxxLibs {
-          name = "libgourou";
-          src = gourou-src;
-          files = [
-            "${gourou-src}/src/libgourou.cpp"
-            "${gourou-src}/src/user.cpp"
-            "${gourou-src}/src/device.cpp"
-            "${gourou-src}/src/fulfillment_item.cpp"
-            "${gourou-src}/src/loan_token.cpp"
-            "${gourou-src}/src/bytearray.cpp"
-            "${pugixml-src}/src/pugixml.cpp"
+        libzipStatic = pkgsFor.pkgsStatic.libzip.overrideAttrs ( prev: {
+          cmakeFlags = ( prev.cmakeFlags or [] ) ++ [
+            "-DBUILD_SHARED_LIBS=OFF"
+            "-DBUILD_EXAMPLES=OFF"
+            "-DBUILD_DOC=OFF"
+            "-DBUILD_TOOLS=OFF"
+            "-DBUILD_REGRESS=OFF"
           ];
-          includes = [
-            "${base64}/include"
-            "${gourou-src}/include"
-            "${pugixml-src}/src"
-            "${updfparser-src}/include"
-          ];
-          optArgOverride = support.mkLibOverride libupdfparser "opt";
-          dbgArgOverride = support.mkLibOverride libupdfparser "dbg";
-        };
-
-        utils = support.mkCxxArchives {
-          name = "util";
-          includes = [
-            "${gourou-src}/include"
-            "${pugixml-src}/src"
-            "${pkgsFor.openssl.dev}/include"
-            "${pkgsFor.curl.dev}/include"
-            "${pkgsFor.zlib.dev}/include"
-            "${pkgsFor.libzip.dev}/include"
-          ];
-          files = [
-            "${gourou-src}/utils/drmprocessorclientimpl.cpp"
-            "${gourou-src}/utils/utils_common.cpp"
-          ];
-        };
-
-        toolObjs =
-          let
-            # Yank flags from arbitrary object file in utils.
-            flags = ( builtins.head ( utils.opt.static.files ) ).flags;
-            compileMain = f:
-              support.compileCxx flags "${gourou-src}/utils/${f}.cpp";
-            binObjs = map ( b: { name = b; value = compileMain b; } ) [
-              "acsmdownloader"
-              "adept_activate"
-              "adept_remove"
-              "adept_loan_mgt"
-            ];
-          in map ( x: x.value ) binObjs;
-
-        mkBin = cxxLinkFlags': obj:
-          support.linkCxxExecutable {
-            name = builtins.head ( builtins.split "\\." obj.name );
-            files = [obj.outPath];
-            cxxLinkFlags = ["-Wl,--as-needed"] ++ cxxLinkFlags';
-          };
-
-        binsDsoDeps =
-          let cxxLinkFlags = [
-                utils.opt.static.outPath
-                libgourou.archives.opt.static.outPath
-                libupdfparser.archives.opt.static.outPath
-                "${pkgsFor.libzip}/lib/libzip.so"
-                "${pkgsFor.zlib}/lib/libz.so"
-                "${pkgsFor.openssl.out}/lib/libcrypto.so"
-                "${pkgsFor.curl.out}/lib/libcurl.so"
-              ];
-          in map ( mkBin cxxLinkFlags ) toolObjs;
-
-        libzipStatic = pkgsFor.libzip.overrideAttrs ( prev: {
-          cmakeFlags = ( prev.cmakeFlags or [] ) ++ ["-DBUILD_SHARED_LIBS=OFF"];
+          outputs = ["out"];
         } );
 
-        binsStaticDeps =
-          let cxxLinkFlags = [
-                "-Wl,-Bstatic"
-                utils.opt.static.outPath
-                libgourou.archives.opt.static.outPath
-                libupdfparser.archives.opt.static.outPath
-                "-Wl,--start-group"
-                "${libzipStatic}/lib/libzip.a"
-                "${pkgsFor.pkgsStatic.libnghttp2}/lib/libnghttp2.a"
-                "${pkgsFor.pkgsStatic.libidn2.out}/lib/libidn2.a"
-                "${pkgsFor.pkgsStatic.libunistring}/lib/libunistring.a"
-                "${pkgsFor.pkgsStatic.libssh2}/lib/libssh2.a"
-                "${pkgsFor.pkgsStatic.zstd.out}/lib/libzstd.a"
-                "${pkgsFor.pkgsStatic.zlib}/lib/libz.a"
-                "${pkgsFor.pkgsStatic.openssl.out}/lib/libcrypto.a"
-                "${pkgsFor.pkgsStatic.curl.out}/lib/libcurl.a"
-                "${pkgsFor.pkgsStatic.openssl.out}/lib/libssl.a"
-                #"${pkgsFor.pkgsStatic.stdenv.cc.libc_lib}/lib/libm.a"
-                #"${pkgsFor.pkgsStatic.stdenv.cc.libc_lib}/lib/libc.a"
-                "-static-libgcc"
-                "-static-libstdc++"
-                "-Wl,--end-group"
-              ];
-          in map ( mkBin cxxLinkFlags ) toolObjs;
+        libgourou = pkgsFor.pkgsStatic.stdenv.mkDerivation rec {
+          pname = "libgourou";
+          version = "0.7.1";
+          src = gourou-src;
+          nativeBuildInputs = with pkgsFor.pkgsStatic; [pkg-config];
+          buildInputs = ( with pkgsFor.pkgsStatic; [
+            openssl.dev
+            qt5.qtbase.dev
+            libzipStatic.out
+            curl.dev
+          ] ) ++ [( updfparser.overrideAttrs ( _: { inherit enableDebug; } ) )];
 
-      in rec {
-        gourou = support.mkBinDir "gourou" binsDsoDeps;
-        gourouStatic = support.mkBinDir "gourou-static" binsStaticDeps;
-        inherit libzipStatic;
-        default = gourouStatic;
+          enableDebug = false;
+
+          preConfigure = ''
+            rm -rf lib scripts
+            mkdir lib
+            cp -pr --reflink=auto ${pugixml-src} lib/pugixml
+            cp -pr --reflink=auto ${base64-src} lib/base64
+            chmod -R u+w lib
+          '';
+
+          makeFlags = ["BUILD_SHARED=0" "BUILD_STATIC=1" "STATIC_UTILS=1"];
+
+          CXXFLAGS = [
+            "-static"
+            "-Wall"
+            "-I./include"
+            "-fkeep-inline-functions"
+            #"-fkeep-static-functions"
+            "-fkeep-static-consts"
+            #"-static-libgcc"
+            "-static"
+            "-Wl,-Bstatic"
+          ] ++ ( if enableDebug then ["-ggdb" "-O0"] else ["-O2"] );
+
+          configurePhase = ''
+            runHook preConfigure
+            : "''${enableDebug=0}"
+            test "$enableDebug" != 0 && makeFlagsArray+=( DEBUG=1 )
+            updfparser_CFLAGS="$( pkg-config --cflags updfparser; )"
+            CXXFLAGS+=" $updfparser_CFLAGS"
+            CXXFLAGS+=" -I$PWD/include"
+            CXXFLAGS+=" -I$PWD/lib"
+            CXXFLAGS+=" -I$PWD/lib/pugixml/src"
+            makeFlagsArray+=( CXXFLAGS="$CXXFLAGS" )
+            updfparser_LIBS+="$( pkg-config --variable=libdir updfparser; )"
+            makeFlagsArray+=( UPDFPARSERLIB="$updfparser_LIBS/libupdfparser.a" )
+            runHook postConfigure
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib $out/bin
+            mv -- libgourou.a $out/lib/
+            mv -- utils/{acsmdownloader,adept_{activate,remove,loan_mgt}}  \
+                  $out/bin/
+            runHook postInstall
+          '';
+
+          dontWrapQtApps = true;
+        };
+
+        default = libgourou;
       }
     );
   };
 }
+
